3583d05233426f051030afbf9b0a96877cb21c73
Robin Coutinho <coutinhorobin1425@gmail.com>
2025-08-19 16:49:40 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -296,92 +296,62 @@ export const projects = [
     github: "https://github.com/robincoutinho1425/BookStoreApp",
     
   },
-  // {
-  //   id: 1,
-  //   title: "Vexa",
-  //   date: "Oct 2022 - Jan 2023",
-  //   description:
-  //     "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
-  //   image:
-  //     "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
-  //   tags: [
-  //     "React Js",
-  //     "MongoDb",
-  //     "Node Js",
-  //     "Express Js",
-  //     "Redux",
-  //     "NodeMailer",
-  //   ],
-  //   category: "web app",
-  //   github: "https://github.com/rishavchanda/Project-Management-App",
-  //   webapp: "https://vexa-app.netlify.app/",
-  // },
-  // {
-  //   id: 2,
-  //   title: "Brain Tumor Detection",
-  //   date: "Jan 2023 - Mar 2023",
-  //   description:
-  //     "Preprocessed and augmented the dataset to improve model accuracy, trained the model, created API using model and Python, and used React web app for the project's front end. Achievements: Achieved an accuracy of 99.2% to accurately detect brain tumors from medical images.",
-  //   image:
-  //     "https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
-  //   tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
-  //   category: "machine learning",
-  //   github: "https://github.com/rishavchanda/Brain-Tumor-Detection",
-  //   webapp: "https://brain-tumor.netlify.app/",
-  //   member: [
-  //     {
-  //       name: "Rishav Chanda",
-  //       img: "https://avatars.githubusercontent.com/u/64485885?v=4",
-  //       linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  //       github: "https://github.com/rishavchanda/",
-  //     },
-  //     {
-  //       name: "Upasana Chaudhuri",
-  //       img: "https://avatars.githubusercontent.com/u/100614635?v=4",
-  //       linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
-  //       github: "https://github.com/upasana0710",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: 3,
-  //   title: "Buckoid",
-  //   date: "Dec 2021 - Apr 2022",
-  //   description:
-  //     "App Is Currently In Playstore 100+ Downloads. This Project proposes an “Expense Tracking App”. Keep track of your personal expenses and compare them to your monthly income with the budget planner. It has Google Drive Cloud API for Backup of User Room Database. Made with Kotlin in MVVM Architecture & Live Data.",
-  //   image:
-  //     "https://camo.githubusercontent.com/3ad28aa710d18525f1fc87de056ed53c706d09979589bfd5a773df36653bad38/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
-  //   tags: ["Kotlin", "MVVM", "Room Database", "Google Drive Cloud API"],
-  //   category: "android app",
-  //   github: "https://github.com/rishavchanda/Buckoid-Android-App",
-  //   webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
-  // },
-  // {
-  //   id: 10,
-  //   title: "Job Finding App",
-  //   date: "Jun 2023 - Jul 2023",
-  //   description:
-  //     "A Job Finding App made with React Native, Axios. Users can search for any job coming from API and apply there.",
-  //   image:
-  //     "https://user-images.githubusercontent.com/64485885/255237090-cf798a2c-1b41-4bb7-b904-b5353a1f08e8.png",
-  //   tags: ["React Native", "JavaScript", "Axios"],
-  //   category: "android app",
-  //   github: "https://github.com/rishavchanda/Job-finder-App",
-  //   webapp: "https://github.com/rishavchanda/Job-finder-App",
-  // },
-  // {
-  //   id: 4,
-  //   title: "Whatsapp Clone",
-  //   date: "Jul 2021",
-  //   description:
-  //     "A WhatsApp clone made with React JS, Firebase, and Material UI. It has Phone Authentication, Real-time Database. It has a chat room where users can chat with each other. It has a sidebar where users can see all the chat rooms and can create a new chat room. It has a login page where users can log in with their Google account.",
-  //   image:
-  //     "https://firebasestorage.googleapis.com/v0/b/whatsapp-clone-rishav.appspot.com/o/Screenshot%20(151).png?alt=media&token=48391593-1ef0-4a8c-a92a-eb82bdf38e89",
-  //   tags: ["React Js", "Firebase", "Firestore", "Node JS"],
-  //   category: "web app",
-  //   github: "https://github.com/rishavchanda/Whatsapp-Clone-React-Js",
-  //   webapp: "https://whatsapp-clone-rishav.web.app",
-  // },
+  {
+    id: 1,
+    title: "Wanderlink- Place Explorer",
+    date: "Nov 2024",
+    description:
+      "Developed full-stack app for discovering and sharing places with map interaction and social features.Integrated geocoding API for accurate place coordinates.Used MongoDB geospatial queries and MapBox for location filtering and map display.Technologies: MERN, MapBox, LocationIQ, JWT, REST APIs.",
+    image:
+      "/yourplaces.png.png",
+    category: "web app",
+    github: "https://github.com/robincoutinho1425/",
+  },
+  {
+    id: 2,
+     title: "Two Good Website",
+    date: "Feb 2024",
+    description:
+      " Built a dynamic website using HTML, CSS,JavaScript, GSAP, Locomotive.js, and Swiper.js. Integrated smooth scrolling and advanced animations to enhance user interaction",
+    image:
+      "/Two good.png.png",
+    category: "web app",
+    github: "https://github.com/robincoutinho1425/",
+   
+  },
+  {
+    id: 3,
+     title: "Rejoice Website",
+    date: "Mar 2024",
+    description:
+      " Developed an interactive website with HTML, CSS, JavaScript, GSAP, Locomotive.js, and Swiper.js. Implemented advanced animations and smooth scrolling for enhanced user experience",
+    image:
+      "/rejoice.png.png",
+    category: "web app",
+    github: "https://github.com/robincoutinho1425/",
+  },
+  {
+    id: 10,
+     title: "Copychecker",
+    date: "Feb 2025",
+    description:
+      " Built a 3-in-1 plagiarism tool using React frontend, Flask backend, and ML models.Compared user-uploaded files via word/line matching with edge-case handling and highlighted matches.Detected AI-generated text in real time using an ensemble ML model (NB, LR, RF) with 92% accuracy.Used Google Programmable Search API and fuzzy matching to find and report web plagiarism sources.Technologies: React, Flask, Python, scikit-learn, Pickle, Google Search API.",
+    image:
+      "/plagcheck.png.png",
+    category: "machine learning",
+    github: "https://github.com/robincoutinho1425/",
+  },
+  {
+    id: 4,
+    title: "Todo Web App",
+   date: "Mar 2024",
+    description:
+      "Developed a responsive Todo App using React for task management. Implemented features to add, edit, delete, and mark tasks as complete. Utilized React hooks for state management and component reusability. Enhanced UI with filtering options (All, Active, Completed) for better user experience.",
+    image:
+      "/todo.png.png",
+    category: "machine learning",
+    github: "https://github.com/robincoutinho1425/",
+  },
   // {
   //   id: 5,
   //   title: "Todo Web App",
